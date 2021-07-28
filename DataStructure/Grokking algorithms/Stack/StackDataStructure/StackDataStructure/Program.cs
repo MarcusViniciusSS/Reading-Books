98eb5bb1f0df9f3e44472c45f6cacbe448f07b3a
Marcus Vinicius Santana Silva <marcus.silva@xpi.com.br>
2021-07-28 14:58:13 -0300

    🆕 Example stack with c#

---
+++ DataStructure/Grokking algorithms/Stack/StackDataStructure/StackDataStructure/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackDataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<string>(1);
+            
+            stack.Push("item 1");
+            stack.Push("item 2");
+            stack.Push("item 3");
+            
+            Console.WriteLine("[Peek] Element: {0}", stack.Peek()); // Peek - 
+
+            var count = stack.Count;
+            for (var i = 0; i < count; i++)
+            {
+                Console.WriteLine("[Pop] Element: {0}", stack.Pop()); // Pop
+            }
+        }
+    }
+}
